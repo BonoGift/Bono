e84d90f2bef4a58a472fe6cad873927f0a1933c6
choycheewei <123choy123@gmail.com>
2022-03-05 19:53:00 +0800

    new post screen design done

--- lib/views/feeds/add_post.dart
+++ lib/views/feeds/add_post.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
-import 'package:bono_gifts/config/constants.dart';
 import 'package:bono_gifts/provider/feeds_provider.dart';
 import 'package:bono_gifts/views/camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 class AddPost extends StatefulWidget {
   @override
   _AddPostState createState() => _AddPostState();
@@ -14,10 +12,12 @@ class _AddPostState extends State<AddPost> {
   bool isClicked = false;
   GlobalKey<FormState> keyy = GlobalKey<FormState>();
   String photoErro = '';
+
   @override
   Widget build(BuildContext context) {
     final pro = Provider.of<FeedsProvider>(context);
     return Scaffold(
+      backgroundColor: const Color(0xffeef9ff),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -29,112 +29,255 @@ class _AddPostState extends State<AddPost> {
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const[
-                      Text("Add Post",style: TextStyle(fontSize: 22),),
+                    children: const [
+                      Text(
+                        "Post Something",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontFamily: 'EdwardianScriptITC',
+                        ),
+                      ),
                     ],
                   ),
-                  const SizedBox(height: 20,),
-                  const Text("Title of post"),
-                  const SizedBox(height: 10,),
+                  const SizedBox(height: 28),
+                  const Text(
+                    "Title",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  const SizedBox(height: 10),
                   Container(
                     decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10)
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 6,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
                     ),
-                    height: 42,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left:8.0),
-                        child: TextFormField(
-                          validator: (val){
-                            if(val!.isEmpty){
-                              return "Title is Required";
-                            }
-                          },
-                          onChanged: (val){
-                            pro.setTitle(val);
-                            // pro.setName(val);
-                          },
-                          textAlign: TextAlign.center,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Write some title",
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      shadowColor: Colors.grey,
+                      margin: const EdgeInsets.all(0),
+                      child: TextFormField(
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Title is Required";
+                          }
+                        },
+                        onChanged: (val) {
+                          pro.setTitle(val);
+                          // pro.setName(val);
+                        },
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: const EdgeInsets.only(left: 16),
+                          hintText: "My birthday party!",
+                          hintStyle: TextStyle(
+                            color: Colors.grey.withOpacity(0.6),
                           ),
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20,),
-                  const Text("Description"),
-                  const SizedBox(height: 10,),
+                  const SizedBox(height: 40),
+                  RichText(
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Description ',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' (optional)',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 4),
                   Container(
                     decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10)
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 6,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
                     ),
                     height: 150,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left:8.0),
-                        child: TextField(
-                          maxLines: 5,
-                          onChanged: (val){
-                            // pro.setName(val);
-                            pro.setDescription(val);
-                          },
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Write some about post",
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      shadowColor: Colors.grey,
+                      margin: const EdgeInsets.all(0),
+                      child: TextField(
+                        maxLines: 5,
+                        onChanged: (val) {
+                          // pro.setName(val);
+                          pro.setDescription(val);
+                        },
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: const EdgeInsets.only(left: 16, top: 16),
+                          hintText: "Leave a description of your post",
+                          hintStyle: TextStyle(
+                            color: Colors.grey.withOpacity(0.6),
                           ),
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20,),
-                  pro.bytesImage == null ? InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const CameraScreen()));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      color: Colors.blue,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RotatedBox(
-                            quarterTurns: 4,
-                            child: Image.asset(photoIcon,height: 30,),
-                          ),
-                          const SizedBox(width: 10,),
-                          const Text("Upload Photos",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500,color: Colors.white),)
-                        ],
+                  const SizedBox(height: 20),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/icons/camera_icon.png',
+                        width: 24,
+                        height: 24,
+                        color: Colors.black.withOpacity(0.6),
                       ),
-                    ),
-                  ):Image.memory(pro.bytesImage!,height: 220,fit: BoxFit.cover,width: double.infinity,),
-                  Text(photoErro,style: TextStyle(color: Colors.red),),
-                  const SizedBox(height: 20,),
+                      const SizedBox(width: 8),
+                      const Text(
+                        'Add photos',
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CameraScreen(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 12),
+                              Image.asset(
+                                'assets/images/icons/post_camera_icon.jpeg',
+                                width: 28,
+                                height: 28,
+                              ),
+                              const SizedBox(height: 4),
+                              const Text(
+                                'Add',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      if (pro.bytesImage != null)
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Image.memory(
+                              pro.bytesImage!,
+                              height: 75,
+                              fit: BoxFit.cover,
+                              width: 75,
+                            ),
+                            Positioned(
+                              top: -6,
+                              right: -6,
+                              child: InkWell(
+                                onTap: () {
+                                  pro.bytesImage = null;
+                                  setState(() {});
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.all(2),
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlue.withOpacity(0.3),
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: const Icon(
+                                    Icons.close,
+                                    size: 12,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    photoErro,
+                    style: const TextStyle(color: Colors.red),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       TextButton(
-                        onPressed: ()=>Navigator.pop(context),
-                        child: const Text("Cancel"),
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.6),
+                            fontSize: 18,
+                          ),
+                        ),
                       ),
+                      const SizedBox(width: 32),
                       MaterialButton(
-                        color: Colors.blue,
-                        onPressed: isClicked ? null :(){
-                          if(keyy.currentState!.validate() || pro.image != null){
-                            setState(() {
-                              isClicked = true;
-                            });
-                            pro.uploadPost(context);
-                          }else{
-                            setState(() {
-                              photoErro = "Please Select Photo";
-                            });
-                          }
-                        },
-                        child: isClicked ? const CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white),):const Text("Post",style: TextStyle(color: Colors.white),),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                        color: const Color(0xff3a3839),
+                        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32),
+                        onPressed: isClicked
+                            ? null
+                            : () {
+                                if (keyy.currentState!.validate() || pro.image != null) {
+                                  setState(() {
+                                    isClicked = true;
+                                  });
+                                  pro.uploadPost(context);
+                                } else {
+                                  setState(() {
+                                    photoErro = "Please Select Photo";
+                                  });
+                                }
+                              },
+                        child: isClicked
+                            ? const CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                              )
+                            : const Text(
+                                "Post",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
                       )
                     ],
                   )
